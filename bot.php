0c23b202e7c63ec84e97420e51d05f1e7aa781c4
icekunx <icekunxp@gmail.com>
2021-01-13 15:08:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,18 +20,46 @@ if ( sizeof($request_array['events']) > 0 ) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
 
-        $text = $event['message']['text'];
-        $data = [
-            'replyToken' => $reply_token,
-            // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => [['type' => 'text', 'text' => $text ]]
-        ];
-        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+ //       $text = $event['message']['text'];
+ //      $data = [
+ //           'replyToken' => $reply_token,
+ //           // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+ //           'messages' => [['type' => 'text', 'text' => $text ]]
+ //       ];
+ //       $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+//
+//       $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+//
+//        echo "Result: ".$send_result."\r\n";
+//    }
+//}
+          if ( $event['type'] == 'message' ) 
+  {
+   if( $event['message']['type'] == 'text' )
+   {
+    $text = $event['message']['text'];
+    $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
+   }
+   else
+    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+  
+  }
+  else
+   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+ 
+  if( strlen($reply_message) > 0 )
+  {
+   //$reply_message = iconv("tis-620","utf-8",$reply_message);
+   $data = [
+    'replyToken' => $reply_token,
+    'messages' => [['type' => 'text', 'text' => $reply_message]]
+   ];
+   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-        echo "Result: ".$send_result."\r\n";
-    }
+   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+   echo "Result: ".$send_result."\r\n";
+  }
+ }
 }
 
 echo "OK";
