6b81227078b1a12a740c036765b85c369cd9f660
icekunx <icekunxp@gmail.com>
2021-01-15 15:59:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -103,7 +103,7 @@ $data = [
     }
     
 else if( $event['message']['text'] == 'ขอดูภาพ' ) {
-$text = 'https://www.picz.in.th/image/lqZB0R';
+$text = 'https://sv1.picz.in.th/images/2021/01/15/lqZB0R.png';
 $text2 = 'https://sv1.picz.in.th/images/2021/01/15/lqZB0R.png';
 $data = ['replyToken' => $reply_token,'messages' => [['type' => 'image', 'originalContentUrl' => $text ,'previewImageUrl' => $text2 ]]
         ];
